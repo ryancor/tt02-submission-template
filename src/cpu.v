9afeb49882ac121f7c9f2decf53a8959081d25b7
ryancor <ryan_cornateanu@yahoo.com>
2022-10-31 14:22:37 -0700

    Update CPU to 8bit #2

--- src/cpu.v
+++ src/cpu.v
@@ -1,60 +1,59 @@
 `default_nettype none
 
 module cpu(
-    input [8:0] INSTRUCTION,
-    input       write_en,
-    input       CLK, RESET,
-    output      PC
+  input [8:0] INSTRUCTION,
+  input       write_en,
+  input       CLK, RESET,
+  output [8:0] PC
 );
 
-reg PC;
-wire PCRESULT;
-reg [8:0] accumulator;
-reg [8:0] input_reg;
+  wire [8:0] PCRESULT;
+  reg [8:0] accumulator;
+  reg [8:0] input_reg;
 
-wire [9:0] sum;
+  wire [9:0] sum;
 
-reg isAdd;
-reg isImediate;
-reg [7:0] immediateVal;
-wire [7:0] mux1out;
-wire [7:0] mux2out;
-wire [7:0] minusVal;
-reg [7:0] IN;
-wire [7:0] OUT2;
+  reg isAdd;
+  reg isImediate;
+  reg [7:0] immediateVal;
+  wire [7:0] mux1out;
+  wire [7:0] mux2out;
+  wire [7:0] minusVal;
+  reg [7:0] IN;
+  wire [7:0] OUT2;
 
-assign sum = input_reg + accumulator;
-assign PC = sum[9];
+  assign sum = input_reg + accumulator;
+  assign PC = sum[9];
 
-// adder to update pc from 4
-adder myadder(PC, PCRESULT);
-always @(posedge CLK) begin
-   PC = PCRESULT;
-end
+  // adder to update pc from 4
+  adder myadder(PC, PCRESULT);
+  // always @(posedge CLK) begin
+  //    PC = PCRESULT;
+  // end
 
-always @(posedge CLK or posedge RESET) begin
-    if (RESET) begin
-        input_reg <= 9'h00 ;
-        accumulator <= 9'h00;
-    end else begin
-        accumulator <= sum[8:0];
-	      if (write_en) begin
-          input_reg <= INSTRUCTION;
-        end
-    end
-end
+  always @(posedge CLK or posedge RESET) begin
+      if (RESET) begin
+          input_reg <= 9'h00 ;
+          accumulator <= 9'h00;
+      end else begin
+          accumulator <= sum[8:0];
+  	      if (write_en) begin
+            input_reg <= INSTRUCTION;
+          end
+      end
+  end
 
-//multiplexer to choose between minus value and plus value
-mux2_1 mymux1(OUT2, minusVal, isAdd, mux1out);
+  //multiplexer to choose between minus value and plus value
+  mux2_1 mymux1(OUT2, minusVal, isAdd, mux1out);
 
-//multiplexer to chose between immediate value and mux1 output
-mux2_1 mymux2(immediateVal, mux1out, isImediate, mux2out);
+  //multiplexer to chose between immediate value and mux1 output
+  mux2_1 mymux2(immediateVal, mux1out, isImediate, mux2out);
 
 endmodule
 
 module adder(
-  input [31:0] PCINPUT,
-  output [31:0] RESULT
+  input [8:0] PCINPUT,
+  output [8:0] RESULT
 );
 
   reg RESULT;
@@ -82,3 +81,4 @@ module mux2_1(
     end
   end
 endmodule
+
