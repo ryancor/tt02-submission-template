19877457009569b1522b41afe12120559375ebfa
ryancor <ryan_cornateanu@yahoo.com>
2022-11-07 06:55:39 -0800

    Add RegFile To CPU

--- src/cpu.v
+++ src/cpu.v
@@ -21,9 +21,13 @@ module cpu(
   wire [7:0] mux2out;
   wire [7:0] minusVal;
   reg [7:0] IN;
+  wire [7:0] OUT1;
   wire [7:0] OUT2;
 
   reg [7:0] OPCODE;
+  reg [2:0] DESTINATION;
+  reg [2:0] SOURCE1;
+  reg [2:0] SOURCE2;
 
   assign sum = input_reg + accumulator;
   assign PC = sum[9];
@@ -50,22 +54,40 @@ module cpu(
     OPCODE = INSTRUCTION[4:1];
     case (OPCODE)
       8'b00000000: begin
-      aluOp = 3'b000;
-      isAdd = 1'b1;
-      isImediate = 1'b1;
+        aluOp = 3'b000;
+        isAdd = 1'b1;
+        isImediate = 1'b1;
       end
       8'b00000001: begin
-      aluOp = 3'b000;
-      isAdd = 1'b1;
-      isImediate = 1'b0;
+        aluOp = 3'b000;
+        isAdd = 1'b1;
+        isImediate = 1'b0;
       end
       8'b00000010: begin
-      aluOp = 3'b001;
-      isAdd = 1'b1;
-      isImediate = 1'b0;
+        aluOp = 3'b001;
+        isAdd = 1'b1;
+        isImediate = 1'b0;
+      end
+      8'b00000011: begin
+        aluOp = 3'b001;
+        isAdd = 1'b0;
+        isImediate = 1'b0;
+      end
+      8'b00000100: begin
+        aluOp = 3'b010;
+        isAdd = 1'b1;
+        isImediate = 1'b0;
+      end
+      8'b00000101: begin
+        aluOp = 3'b011;
+        isAdd = 1'b1;
+        isImediate = 1'b0;
       end
     endcase
   end
+
+  // including the registers
+  reg_file myReg(IN, OUT1, OUT2, DESTINATION, SOURCE1, SOURCE2, write_en, CLK, RESET);
 
   //multiplexer to choose between minus value and plus value
   mux2_1 mymux1(OUT2, minusVal, isAdd, mux1out);
@@ -106,3 +128,34 @@ module mux2_1(
   end
 endmodule
 
+module reg_file(
+  input [7:0] IN,
+  output [7:0] OUT1,
+  output [7:0] OUT2,
+  input [2:0] INADDRESS,
+  input [2:0] OUT1ADDRESS,
+  input [2:0] OUT2ADDRESS,
+  input WRITE,
+  input CLK,
+  input RESET
+);
+
+  integer i;
+  reg [7:0] regFile [0:7];
+  always @(*) begin
+    if (RESET == 1) begin
+      for (i = 0; i < 8; i = i + 1) begin
+        regFile[i] = 8'b00000000;
+      end
+    end
+  end
+
+  always @(posedge CLK) begin
+    if (WRITE == 1'b1 && RESET == 1'b0) begin
+      regFile[INADDRESS] = IN;
+    end
+  end
+
+  assign OUT1 = regFile[OUT1ADDRESS];
+  assign OUT2 = regFile[OUT2ADDRESS];
+endmodule
