695bdbbf4d8fbb41b4c75e322655a6617ab4fbaa
ryancor <ryan_cornateanu@yahoo.com>
2022-11-09 19:28:58 -0800

    pin out for the top module

--- src/top.v
+++ src/top.v
@@ -3,13 +3,6 @@ module top (
 	output [17:0] io_out
 );
 	
-	input [8:0] INSTRUCTION,
-  input       write_en,
-  input       CLK, RESET, RD, CS,
-  input [7:0] INALU,
-  output [8:0] PC,
-  output [7:0] ALURESULT
-	
 	cpu cpu(.INSTRUCTION(io_in[8:0]), .write_en(io_in[9]), .CLK(io_in[10]), 
 					.RESET(io_in[11]), .RD(io_in[12]), .CS(io_in[13]), .INALU(io_in[22:14]),
 					.PC(io_out[8:0]), .ALURESULT(io_out[17:9])
