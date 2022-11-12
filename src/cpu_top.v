9b17a1350939a798ae17d565e6988f8a69d52f8a
ryancor <ryan_cornateanu@yahoo.com>
2022-11-12 11:54:06 -0800

    Remove unnecessary pins from cpu main

--- src/cpu_top.v
+++ src/cpu_top.v
@@ -3,9 +3,8 @@ module cpu_top (
 	output [7:0] io_out
 );
 	
-	cpu cpu(.INSTRUCTION(io_in[2:1]), .write_en(io_in[3]), .CLK(io_in[0]), 
-					.RESET(io_in[4]), .RD(io_in[5]), .CS(io_in[6]), .INALU(io_in[7]),
-					.PC(io_out[3:0]), .ALURESULT(io_out[7:4])
+	cpu cpu(.INSTRUCTION(io_in[3:1]), .write_en(io_in[4]), .CLK(io_in[0]), 
+					.RESET(io_in[5]), .RD(io_in[6]), .CS(io_in[7]), .PC(io_out[7:0])
 				 );
 	
 endmodule
