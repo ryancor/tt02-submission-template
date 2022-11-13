abd909169ce95cc0bffddc7930c3d6a8319870f7
ryancor <ryan_cornateanu@yahoo.com>
2022-11-12 16:42:53 -0800

    remove ZERO2 from ALU

--- src/cpu.v
+++ src/cpu.v
@@ -236,8 +236,7 @@ module alu(
   output [7:0] PC,
   input [2:0] SELECT,
   input [7:0] RshiftResult,
-  output ZERO,
-  output ZERO2
+  output ZERO
 );
 
   reg [7:0] RESULT;
@@ -282,11 +281,7 @@ module alu(
  // creating the ZERO bit using the alu result
  //modified part
  always @(RESULT) begin
-  ZERO = RESULT[0]~|RESULT[1]~|RESULT[2]~|RESULT[3]~|RESULT[4]~|RESULT[5]~|RESULT[6]~|RESULT[7];
- end
-
- always @(PC) begin
-  ZERO2 = PC[0]~|PC[1]~|PC[2]~|PC[3]~|PC[4]~|PC[5]~|PC[6]~|PC[7];
+  ZERO = RESULT[0]~|PC[1]~|RESULT[2]~|PC[3]~|RESULT[4]~|RESULT[5]~|PC[6]~|RESULT[7];
  end
 endmodule
 
