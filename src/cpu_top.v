17eee548e471d5bfe53508e27ffd404bfc6b7764
ryancor <ryan_cornateanu@yahoo.com>
2022-11-12 17:08:07 -0800

    add data pin

--- src/cpu_top.v
+++ src/cpu_top.v
@@ -4,7 +4,8 @@ module cpu_top (
 );
 	
 	cpu cpu(.INSTRUCTION(io_in[3:1]), .write_en(io_in[4]), .CLK(io_in[0]), 
-					.RESET(io_in[5]), .RD(io_in[6]), .CS(io_in[7]), .PC(io_out[7:0])
+					.RESET(io_in[5]), .RD(io_in[6]), .CS(io_in[7]), .PC(io_out[7:4], 
+					.DATA(io_out[3:0])
 				 );
 	
 endmodule
