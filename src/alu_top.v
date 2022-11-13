a1ddafaf4d4c0309ac6884301d15aee267492bcb
ryancor <ryan_cornateanu@yahoo.com>
2022-11-12 18:05:53 -0800

    Update and rename cpu_top.v to alu_top.v

--- src/alu_top.v
+++ src/alu_top.v
@@ -1,9 +1,9 @@
-module cpu_top (
+module alu_top (
 	input [7:0] io_in,
 	output [7:0] io_out
 );
 	
-	cpu cpu(.A(io_in[7:6]), .B(io_in[5:4]), .ALU_Sel(io_in[3:0]), 
+	alu alu(.A(io_in[7:6]), .B(io_in[5:4]), .ALU_Sel(io_in[3:0]), 
 		.ALU_Out(io_out[6:0]), .CarryOut(io_out[7])
   );
 	
